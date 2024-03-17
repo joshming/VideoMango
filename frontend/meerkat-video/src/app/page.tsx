f77e361a8ae2be48d8048a70e2c00d6ddfccabe4
joshming <josh.jm2011@gmail.com>
2024-03-16 20:39:43 -0400

    add links to the new pages

--- frontend/meerkat-video/src/app/page.tsx
+++ frontend/meerkat-video/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Video from "@/app/_components/VideoComponent";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex justify-center">
-
+      <Link href={`/login`}> Login </Link>
+      <Link href={`/signup`}> Sign Up </Link>
     </main>
   );
 }
