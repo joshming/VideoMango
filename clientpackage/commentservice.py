93baa9d9ffd745f523719fde58c0bdadffb88abb
Joshua Ming <60520554+joshming@users.noreply.github.com>
2024-04-01 10:56:49 -0400

    feature commenting (#3)
    * feature commenting
    
    * renamed chatpacakge to properly reflect comment usages and improved comment front end

---
+++ clientpackage/commentservice.py
@@ -0,0 +1,32 @@
+import grpc
+
+from comment.Comment import Comment, Message
+from proto import commentserver_pb2_grpc, commentserver_pb2
+
+CHAT_PORT = "50070"
+
+
+def send_message(video_id: int, user: str, message: str):
+    # to make this infinite, maybe the channel cannot be closed,
+    with grpc.insecure_channel('localhost:' + CHAT_PORT) as channel:
+        stub = commentserver_pb2_grpc.CommentServerStub(channel)
+        stub.send_message(
+            commentserver_pb2.SendComment(video_id=video_id, comment=commentserver_pb2.Comment(user=user, message=message)))
+
+
+def get_messages(video_id: int) -> Comment:
+    with grpc.insecure_channel('localhost:' + CHAT_PORT) as channel:
+        stub = commentserver_pb2_grpc.CommentServerStub(channel)
+        users = set()
+        comments = Comment([], [], video_id)
+        comments_iterator = stub.get_comments(commentserver_pb2.Video(video_id=video_id))
+        for comment in comments_iterator:
+            if comment.user == "":
+                return comments
+            message = Message(comment.user, comment.message, int(comment.time))
+            if comment.user not in users:
+                users.add(comment.user)
+                comments.users.append(comment.user)
+            comments.messages.append(message)
+
+        return comments
---
+++ frontend/meerkat-video/src/app/_components/Comment.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import {useEffect, useState} from "react";
+
+type message = {
+    user: string
+    message: string
+    time: string
+}
+
+type Comments = {
+    users: string[]
+    messages: message[]
+}
+
+// interval is in milliseconds -- default is 5000ms, change it as you want to
+const FETCH_DATA_INTERVAL = 5000;
+
+export default function Comment({id}: { id: number }) {
+
+    const [comments, setComments] = useState<JSX.Element[] | null>(null);
+    const [newComment, setNewComment] = useState<string>('');
+    const [refresh, setRefresh] = useState(false);
+    const fetchData = async () => {
+        try {
+            console.log("fetching...")
+            const response = await fetch(`http://localhost:3000/api/comments?id=${id}`, {
+                method: 'GET'
+            });
+            const data: Comments = await response.json();
+            const commentJSX = data.messages.map(message => {
+                return (
+                    <div className={`grid grid-cols-4 grid-rows-2 w-full mb-2`}>
+                        <h3 className={`col-span-4 row-span-1 m-0 font-bold`}>{message.user}:</h3>
+                        <p className={`col-span-3`}>{message.message}</p>
+                        <p className={`col-span-1`}>{message.time}</p>
+                    </div>
+                )
+            })
+            setComments(commentJSX);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const postComment = async () => {
+        try {
+            const pythonSafeString = newComment.replace("'", "`");
+            console.log(pythonSafeString);
+            const postBody = { "message": pythonSafeString}
+            const response = await fetch(`http://localhost:3000/api/comments?id=${id}`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(postBody)
+            })
+
+            if (!response.ok) {
+                console.log("could not post comment")
+                return;
+            }
+            setNewComment('');
+            setRefresh(true);
+        } catch (error) {
+            console.error('Error posting comment', error);
+        }
+    };
+
+    useEffect(() => {
+            fetchData();
+            const intervalId = setInterval(fetchData, FETCH_DATA_INTERVAL);
+            return () => clearInterval(intervalId);
+        }, []
+    );
+
+    useEffect(() => {
+        console.log("refreshing")
+        fetchData();
+    }, [refresh]);
+
+    return (
+        <div className={`w-full`}>
+            <div className={'grid grid-cols-12 bg-opacity-50 bg-amber-200 rounded-2xl p-5 w-full'}>
+                <input className={'col-span-11 rounded-xl p-3 focus:border-0'}
+                       placeholder="New Comment"
+                       value={newComment}
+                       onChange={e => setNewComment(e.currentTarget.value)}
+                >
+                </input>
+                <button className={`col-span-1 bg-tangerine hover:bg-orange-400 ml-3 rounded-md`}
+                        onClick={postComment}
+                >
+                    Send
+                </button>
+            </div>
+            <div className={`max-h-36 overflow-y-scroll mt-10`}>
+                {
+                    comments && comments.map(e => e)
+                }
+            </div>
+        </div>
+
+    )
+}
