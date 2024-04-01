ed1280774c81f8d64a474688cb87b8ca198e18e2
Joshua Ming <60520554+joshming@users.noreply.github.com>
2024-04-01 10:56:49 -0400

    feature commenting (#3)
    * feature commenting
    
    * renamed chatpacakge to properly reflect comment usages and improved comment front end

--- frontend/meerkat-video/tailwind.config.ts
+++ frontend/meerkat-video/tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "off-white": "#fff1f0",
+        "tangerine": "rgb(252, 196, 99)",
+      }
     },
   },
   plugins: [],
---
+++ startchatserver.py
@@ -0,0 +1,6 @@
+import sys
+
+from comment import commentserver
+
+if __name__ == '__main__':
+    commentserver.main(sys.argv[1])
