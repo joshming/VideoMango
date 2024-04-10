962c9a8da9b41c292daa989d90519c35c13496ad
Bartek K <bartekkatarzynski@hotmail.com>
2024-04-09 22:05:44 -0400

    login disappears when logged in

--- frontend/meerkat-video/src/app/_components/Login.tsx
+++ frontend/meerkat-video/src/app/_components/Login.tsx
@@ -67,7 +67,7 @@ export default function Login() {
 
     return (
         <>
-            <button className="navbutton" onClick={openModal}>Log in</button>
+            {!isSuccess && <button className="navbutton" onClick={openModal}>Log in</button>}
             {showModal &&
                 <div className="modal">
                     <div className="modalbox" id="modal-box">
